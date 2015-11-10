48ae893e83bedd33d7cab1cdcb99ee5064ce5373
jaymmodi <modi.jay6@gmail.com>
2015-11-10 00:20:20 -0500

    adding basic User model

---
+++ src/main/java/Model/User.java
@@ -0,0 +1,45 @@
+package Model;
+
+/**
+ * Created by jmmodi on 11/10/2015.
+ */
+public class User {
+
+    public String firstName;
+    public String lastName;
+    public String emailAddress;
+    public Long phoneNumber;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
