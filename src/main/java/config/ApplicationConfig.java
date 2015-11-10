715276eda7dba5a684e1fbd469eff4a8b8e688f4
jaymmodi <modi.jay6@gmail.com>
2015-11-09 23:02:09 -0500

    adding view resolver for thymeleaf

--- src/main/java/config/ApplicationConfig.java
+++ src/main/java/config/ApplicationConfig.java
@@ -1,11 +1,15 @@
 package config;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 /**
  * Created by jmmodi on 11/9/2015.
@@ -13,14 +17,35 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableAsync
-@ComponentScan(basePackages =  {"controllers"})
+@ComponentScan(basePackages = {"controllers"})
 public class ApplicationConfig {
 
+
     @Bean
-    public ViewResolver getViewResolver(){
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+
+        return viewResolver;
+    }
+
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine bean = new SpringTemplateEngine();
+        bean.setTemplateResolver(templateResolver());
+        bean.addDialect(new LayoutDialect());
+        return bean;
+    }
+
+    @Bean
+    public TemplateResolver templateResolver() {
+        ServletContextTemplateResolver bean = new ServletContextTemplateResolver();
+        bean.setCacheable(false);
+        bean.setTemplateMode("HTML5");
+        bean.setPrefix("/WEB-INF/templates/");
+        bean.setSuffix(".html");
+        return bean;
     }
 }
