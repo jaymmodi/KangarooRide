83d0e8f41ab268c0260d029e3222b099fb7ef633
jaymmodi <modi.jay6@gmail.com>
2015-11-11 18:23:10 -0500

    send date selected by user to server to get time slots for that date using ajax and jquery

---
+++ src/main/webapp/resources/js/sendDateToServer.js
@@ -0,0 +1,19 @@
+function sendDate() {
+    var date = $('#datepicker').val();
+    $.ajax({
+        method: "GET",
+        url: '/getSlots',
+        data: {
+            date: date,
+        },
+        success : function(data){
+            $('#timeSlots').empty();
+            $.each(data, function(key, value) {
+                $('#timeSlots')
+                    .append($("<option></option>")
+                        .attr("value",key)
+                        .text(value));
+            });
+        }
+    });
+}
