d5bc3ab6794e3fb165149eadaea46272bdaa57f6
jaymmodi <modi.jay6@gmail.com>
2015-11-13 16:46:11 -0500

    adding date time service class to map all database time slots to list of string time slots to display on form.html

--- src/main/java/com/ride/controllers/FormController.java
+++ src/main/java/com/ride/controllers/FormController.java
@@ -47,13 +47,7 @@ public class FormController {
     @RequestMapping(value = "/getSlots", method = RequestMethod.GET)
     @ResponseBody
     private ArrayList<String> getTimeSlots(@RequestParam("date") String date) {
-        ArrayList<String> s;
-//        s.add("8.00 AM- 8.30 AM");
-//        s.add("8.30 AM- 9:00 AM");
-
-        s = dateTimeSlotService.getTimeSlotsFor(date);
-
-        return s;
+        return dateTimeSlotService.getTimeSlotsFor(date);
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
--- src/main/java/com/ride/services/DateTimeSlotService.java
+++ src/main/java/com/ride/services/DateTimeSlotService.java
@@ -1,10 +1,13 @@
 package com.ride.services;
 
 import com.ride.DAO.DateTimeDAO;
+import com.ride.Model.StartEndSlot;
+import com.ride.Model.TimeSlot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jmmodi on 11/13/2015.
@@ -17,6 +20,120 @@ public class DateTimeSlotService {
     DateTimeDAO dateTimeDAO;
 
     public ArrayList<String> getTimeSlotsFor(String date) {
-        return dateTimeDAO.getSlotsFor(date);
+
+        int rows = dateTimeDAO.getRowCountForThisDate(date);
+        StartEndSlot startEndSlot = dateTimeDAO.getStartSlot();
+
+        if (rows > 0) {
+            List<TimeSlot> timeSlotList = dateTimeDAO.getSlotsFor(date);
+            return convertToListString(timeSlotList, startEndSlot);
+        } else {
+            return allSlots(startEndSlot);
+        }
+    }
+
+    private ArrayList<String> allSlots(StartEndSlot startEndSlot) {
+        int hourDiff = hourDiff(startEndSlot.getEndHour(), startEndSlot.getStartHour(), startEndSlot.getStartMeridian(), startEndSlot.getEndMeridian());
+        int minDiff = minDiff(startEndSlot.getEndMin(), startEndSlot.getStartMin());
+
+        int totalMins = hourDiff * 60 + minDiff;
+        int totalNumberOfSlots = totalMins / startEndSlot.getDuration();
+        List<TimeSlot> timeSlots = new ArrayList<>();
+
+        for (int i = 1; i <= totalNumberOfSlots; i++) {
+            TimeSlot timeSlot = new TimeSlot();
+            timeSlot.setSlotNumber(i);
+            timeSlots.add(timeSlot);
+        }
+        return convertToListString(timeSlots, startEndSlot);
+
+    }
+
+    private int minDiff(int endMin, int startMin) {
+        int diff = endMin - startMin;
+        if (diff < 0) {
+            return 60 - diff;
+        }
+        return diff;
+    }
+
+    private int hourDiff(int endHour, int startHour, String startMeridian, String endMeridian) {
+        if (endMeridian.equals("PM")) {
+            endHour = endHour + 12;
+        }
+        if (startMeridian.equals("PM")) {
+            startHour = startHour + 12;
+        }
+        return endHour - startHour;
+    }
+
+    private ArrayList<String> convertToListString(List<TimeSlot> timeSlotList, StartEndSlot startEndSlot) {
+        List<String> slots = new ArrayList<>();
+        String slotInString;
+
+        for (TimeSlot timeSlot : timeSlotList) {
+            slotInString = makeTimeSlots(timeSlot, startEndSlot);
+            slots.add(slotInString);
+        }
+        return (ArrayList<String>) slots;
+    }
+
+    private String makeTimeSlots(TimeSlot timeSlot, StartEndSlot startEndSlot) {
+
+        int slotNumber = timeSlot.getSlotNumber();
+
+        int startHour = startEndSlot.getStartHour();
+        int startMin = startEndSlot.getStartMin() + (slotNumber - 1) * startEndSlot.getDuration();
+        String startMeridian = "AM";
+        String endMeridian = "AM";
+
+        if (startMin > 60) {
+            startHour = startHour + (startMin / 60);
+            if (startMin % 60 == 0) {
+                startMin = 0;
+            } else {
+                startMin = startEndSlot.getDuration();
+            }
+        }
+
+        int endMin = startMin + startEndSlot.getDuration();
+        int endHour = startHour;
+
+        if (endMin == 60) {
+            endHour++;
+            endMin = 0;
+        }
+
+        startHour = day12HourClock(startHour);
+        startMeridian = getMeridian(startHour, startMeridian);
+
+        endHour = day12HourClock(endHour);
+        endMeridian = getMeridian(endHour, endMeridian);
+
+        setToTimeSlot(timeSlot, startHour, startMin);
+
+        return String.format("%d.%02d%s-%d.%02d%s", startHour, startMin, startMeridian, endHour, endMin, endMeridian);
+    }
+
+    private void setToTimeSlot(TimeSlot timeSlot, int startHour, int startMin) {
+        timeSlot.setHourStart(startHour);
+        timeSlot.setMinStart(startMin);
+    }
+
+    public int day12HourClock(int hour) {
+        if (hour > 12) {
+            hour = hour % 12;
+        }
+        return hour;
+    }
+
+
+    public String getMeridian(int hour, String meridian) {
+        if (hour >= 12) {
+            meridian = "PM";
+        } else {
+            meridian = "AM";
+        }
+        return meridian;
     }
 }
