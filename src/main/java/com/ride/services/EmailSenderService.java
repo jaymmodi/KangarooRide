070c06fe8b8700dd8ea646e9f943e666a4541e35
jaymmodi <modi.jay6@gmail.com>
2015-11-14 06:28:07 -0500

    using SimpleMailMessage to send emails

--- src/main/java/com/ride/services/EmailSenderService.java
+++ src/main/java/com/ride/services/EmailSenderService.java
@@ -1,12 +1,9 @@
 package com.ride.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
-
-import javax.mail.Message;
-import javax.mail.internet.InternetAddress;
 
 /**
  * Created by jmmodi on 11/12/2015.
@@ -22,13 +19,10 @@ public class EmailSenderService {
     public void sendEmail(final String emailAddress, String code) {
         String text = "Congratulations on booking a ride with Crazy Calvin's Kangaroo Rides and your confirmation Code is " + code;
 
-        MimeMessagePreparator preparator = mimeMessage -> {
-            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(emailAddress));
-            mimeMessage.setFrom("modi.jay6@gmail.com");
-            mimeMessage.setText(text);
-            mimeMessage.setSubject("KangarooRide - ConfirmationCode");
-        };
-
-        this.mailSender.send(preparator);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(emailAddress);
+        message.setSubject("KangarooRide - ConfirmationCode");
+        message.setText(text);
+        mailSender.send(message);
     }
 }
